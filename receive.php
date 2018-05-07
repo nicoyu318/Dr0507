39a869302e76e8eb3f269fcb21a822066ffb57ba
nicoyu318 <39040519+nicoyu318@users.noreply.github.com>
2018-05-07 15:01:40 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -7,19 +7,28 @@
   
   $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
+  $sender_txt=rawurlencode($sender_txt); //因為使用get的方式呼叫luis api，所以需要轉碼
+  $ch = curl_init('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c72546f8-8ea9-403d-a4fb-7203217e4b54?subscription-key=f3ede71b589e4eacbf35f3f7b28cc212&staging=true&verbose=true&timezoneOffset=0&q='.$sender_txt);                                                                      
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                          
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result_str = curl_exec($ch);
+  fwrite($myfile, "\xEF\xBB\xBF".$result_str); //在字串前加上\xEF\xBB\xBF轉成utf8格式
+  $result = json_decode($result_str);
+  $ans_txt = $result -> topScoringIntent -> intent;
   $response = array (
     "to" => $sender_userid,
     "messages" => array (
       array (
         "type" => "text",
-        "text" => "Hello. You say". $sender_txt
+        "text" => $ans_txt
       )
     )
   );
   
+  
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
-
   $header[] = "Content-Type: application/json";
   $header[] = "Authorization: Bearer 5VC2skUsCqRPhgbtIdJR171xKitIUPCROMrSmPoTL6rcAELaj519b1mqiuzBTyOrVWdProJ0CgVXoNhxXRAeN9sclBj4qErZdy8eHINRarkxgDqlrhqTMzGNPMr7uaTXy5+LqdvmlB1Bngjal9jLCAdB04t89/1O/w1cDnyilFU=";
   $ch = curl_init("https://api.line.me/v2/bot/message/push");
